9b7f844af5817d873b898e5a88c63cd57ebb6e0a
Daniel Coles <me@danielcoles.co.uk>
2021-06-17 19:52:32 +0100

    :lipstick: (pokemon) updated styling of navigation component

--- components/pokedex/pokemon/PokemonNavigation.tsx
+++ components/pokedex/pokemon/PokemonNavigation.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Link from 'next/link'
 import { PokemonNavigationData } from '@/types/PokemonData'
-import { getPaddedPokemonId, getPokemonImage, resolvePokemonName } from '@/utils/helpers'
+import { getPaddedPokemonId, resolvePokemonName } from '@/utils/helpers'
 import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from 'react-icons/md'
 
 interface PokemonNavigationProps {
@@ -18,29 +18,34 @@ const PokemonNavigation: React.FC<PokemonNavigationProps> = ({ next, prev }) => 
 
   return (
     <div className="[ POKEMON__NAVIGATION ][ flex ]">
-      <Link href="/pokedex/[slug]" as={`/pokedex/${prev.id}`}>
-        <a className="hover:bg-pink-50 hover:text-page-alt-color p-2 rounded-md">
-          <img src={getPokemonImage(prev.id)} alt={prevName} className="w-16 mx-auto" />
-          <div className="flex">
-            <MdKeyboardArrowLeft size="1.5em" />
-            <p>
-              #{prevId} <span className="capitalize">{prevName}</span>
+      <div className="[ POKEMON__PREV ][ float-left w-full ]">
+        <Link href="/pokedex/[slug]" as={`/pokedex/${prev.id}`}>
+          <a className="hover:bg-pink-50 p-4 rounded-md flex text-5xl">
+            <MdKeyboardArrowLeft size="1.25em" className="text-gray-300" />
+            <p className="font-bold w-full text-left text-gray-300">
+              #{prevId}
+              <span className="hidden md:inline capitalize ">
+                {'  '}
+                {prevName}
+              </span>
             </p>
-          </div>
-        </a>
-      </Link>
-      <div className="mx-auto"></div>
-      <Link href="/pokedex/[slug]" as={`/pokedex/${next.id}`}>
-        <a className="hover:bg-pink-50 hover:text-page-alt-color p-2 rounded-md">
-          <img src={getPokemonImage(next.id)} alt={nextName} className="w-16 mx-auto" />
-          <div className="flex">
-            <p>
-              #{nextId} <span className="capitalize">{nextName}</span>
+          </a>
+        </Link>
+      </div>
+      <div className="[ POKEMON__NEXT ][ float-right w-full ]">
+        <Link href="/pokedex/[slug]" as={`/pokedex/${next.id}`}>
+          <a className="hover:bg-pink-50 p-4 rounded-md flex text-5xl">
+            <p className="font-bold w-full text-right text-gray-300">
+              <span className="hidden md:inline capitalize ">
+                {nextName}
+                {'  '}
+              </span>
+              #{nextId}
             </p>
-            <MdKeyboardArrowRight size="1.5em" />
-          </div>
-        </a>
-      </Link>
+            <MdKeyboardArrowRight size="1.25em" className="text-gray-300" />
+          </a>
+        </Link>
+      </div>
     </div>
   )
 }
