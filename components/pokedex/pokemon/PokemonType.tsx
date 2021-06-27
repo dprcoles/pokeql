ddaf89570822a9b32537b830a7dcd2771351c119
Daniel Coles <me@danielcoles.co.uk>
2021-06-27 22:51:02 +0100

    :sparkles: (pokedex) added type data to pokedex list cards

--- components/pokedex/pokemon/PokemonType.tsx
+++ components/pokedex/pokemon/PokemonType.tsx
@@ -3,13 +3,20 @@ import { PokemonTypeData } from '@/types/PokemonData'
 
 interface PokemonTypeProps {
   type: PokemonTypeData
+  size: string
 }
 
-const PokemonType: React.FC<PokemonTypeProps> = ({ type }) => {
+const PokemonType: React.FC<PokemonTypeProps> = ({ type, size }) => {
   const { name } = type
   return (
     <div className={`[ POKEMON__TYPE ][ Type__${name.toUpperCase()} ][ rounded-md mx-auto ]`}>
-      <p className="capitalize text-2xl font-bold text-center px-4 py-1">{name}</p>
+      <p
+        className={`capitalize ${
+          size === 'small' ? 'text-sm' : 'text-2xl'
+        } font-bold text-center py-1 px-4 `}
+      >
+        {name}
+      </p>
     </div>
   )
 }
