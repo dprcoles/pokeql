f4a4987c787f878896ba0c3f4c7a86b12f73778d
Daniel Coles <me@danielcoles.co.uk>
2022-01-17 21:45:29 +0000

    :mag: (title) added dynamic titles to pages

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -173,3 +173,7 @@ export function getWeightFilter(weights: Array<string>): WeightFilter {
 
   return filter
 }
+
+export function capitalize(string: String): String {
+  return string.charAt(0).toUpperCase() + string.slice(1)
+}
